07ae108e14f697236f6c6672f8e2aed34a31f305
colin <colin@1f95d61e-86ce-8646-bf33-7b98dbf5df5b>
2013-02-07 07:31:08 +0000

    Update script for upgrading

--- Install360Controller/Scripts/upgrade.sh
+++ Install360Controller/Scripts/upgrade.sh
@@ -1,8 +1,38 @@
 #!/bin/sh
-echo "installer got 0: '$0' 1: '$1' 2: '$2' 3: '$3'"
-cd $3
-cd System/Library/Extensions
-kextload 360Controller.kext
-kextunload 360Controller.kext
-# If we can't unload the driver, the user risks kernel panics (not anymore)
+
+# Clear out daemon
+
+if [ -d /Library/StartupItems/360ControlDaemon ]; then
+   /bin/rm -r /Library/StartupItems/360ControlDaemon
+fi
+
+if [ -f /Library/LaunchDaemons/com.mice.360Daemon.plist ]; then
+   launchctl stop com.mice.360Daemon
+   /bin/rm /Library/LaunchDaemons/com.mice.360Daemon.plist
+fi
+
+if [ -d /Library/Application\ Support/MICE/360Daemon ]; then
+   /bin/rm -r /Library/Application\ Support/MICE/360Daemon
+fi
+
+# Remove preference pane
+
+if [ -d /Library/PreferencePanes/Pref360Control.prefPane ]; then
+   /bin/rm -r /Library/PreferencePanes/Pref360Control.prefPane
+fi
+
+# Remove drivers
+
+if [ -d /System/Library/Extensions/360Controller.kext ]; then
+   /bin/rm -r /System/Library/Extensions/360Controller.kext
+fi
+
+if [ -d /System/Library/Extensions/Wireless360Controller.kext ]; then
+   /bin/rm -r /System/Library/Extensions/Wireless360Controller.kext
+fi
+
+if [ -d /System/Library/Extensions/WirelessGamingReceiver.kext ]; then
+   /bin/rm -r /System/Library/Extensions/WirelessGamingReceiver.kext
+fi
+
 exit 0
