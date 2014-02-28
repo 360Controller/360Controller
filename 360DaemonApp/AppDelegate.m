c9637a229e83d2a03ef590382d10c4412cac50a5
C.W. Betts <computers57@hotmail.com>
2014-02-28 11:51:08 -0700

    Move all projects to one Xcode project. TODO: migrate the haptic/force feedback demo/testers. Better version parity.

---
+++ 360DaemonApp/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  360DaemonApp
+//
+//  Created by C.W. Betts on 2/28/14.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
---
+++ 360DaemonApp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  360DaemonApp
+//
+//  Created by C.W. Betts on 2/28/14.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
