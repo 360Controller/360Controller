7ae1ac09352c9ea053590bf8f38933db18a7f210
FranticRain <franticrain@gmail.com>
2015-02-01 15:32:16 -0600

    Major Pref Pane Changes

---
+++ Pref360Control/MyWhole360ControllerMapper.h
@@ -0,0 +1,13 @@
+//
+//  MyWhole360ControllerMapper.h
+//  360 Driver
+//
+//  Created by Drew Mills on 1/30/15.
+//  Copyright (c) 2015 GitHub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MyWhole360ControllerMapper : NSObject
+
+@end
---
+++ Pref360Control/MyWhole360ControllerMapper.m
@@ -0,0 +1,13 @@
+//
+//  MyWhole360ControllerMapper.m
+//  360 Driver
+//
+//  Created by Drew Mills on 1/30/15.
+//  Copyright (c) 2015 GitHub. All rights reserved.
+//
+
+#import "MyWhole360ControllerMapper.h"
+
+@implementation MyWhole360ControllerMapper
+
+@end
