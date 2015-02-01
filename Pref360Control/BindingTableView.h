57a6fce227a15d5ddc6f5ee55658acee4f4e9cd5
FranticRain <franticrain@gmail.com>
2015-02-01 15:32:16 -0600

    Major Pref Pane Changes

---
+++ Pref360Control/BindingTableView.h
@@ -0,0 +1,13 @@
+//
+//  BindingTableView.h
+//  360 Driver
+//
+//  Created by Drew Mills on 1/30/15.
+//  Copyright (c) 2015 GitHub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BindingTableView : NSObject
+
+@end
---
+++ Pref360Control/BindingTableView.m
@@ -0,0 +1,13 @@
+//
+//  BindingTableView.m
+//  360 Driver
+//
+//  Created by Drew Mills on 1/30/15.
+//  Copyright (c) 2015 GitHub. All rights reserved.
+//
+
+#import "BindingTableView.h"
+
+@implementation BindingTableView
+
+@end
