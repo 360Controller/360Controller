a1a3c59adbce949c3b56d11e0075892f408e734a
FranticRain <franticrain@gmail.com>
2018-04-02 20:26:28 -0500

    Fixes to potentially resolve Sierra issues

--- Install360Controller/Scripts/finish.sh
+++ Install360Controller/Scripts/finish.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 #This is to fix a bug in Yosemite.
-/bin/ln -s /Library/Extensions/360Controller.kext /System/Library/Extensions/
+# /bin/ln -s /Library/Extensions/360Controller.kext /System/Library/Extensions/
 
-/usr/bin/touch /System/Library/Extensions
+# /usr/bin/touch /System/Library/Extensions
 /usr/bin/touch /Library/Extensions
+/usr/bin/touch /Library/Preferences/com.mice.driver.Xbox360Controller.devices.plist
 /bin/launchctl load -w /Library/LaunchDaemons/com.mice.360Daemon.plist
 
 exit 0
--- build.sh
+++ build.sh
@@ -21,15 +21,15 @@ xcrun lipo -info build/Release/360Controller.kext/Contents/PlugIns/Feedback360.p
 xcrun lipo -info build/Release/360Daemon.app/Contents/MacOS/360Daemon
 xcrun lipo -info build/Release/Pref360Control.prefPane/Contents/MacOS/Pref360Control
 xcrun lipo -info build/Release/Pref360Control.prefPane/Contents/Resources/DriverTool
-xcrun lipo -info build/Release/WirelessGamingReceiver.kext/Contents/MacOS/WirelessGamingReceiver
-xcrun lipo -info build/Release/Wireless360Controller.kext/Contents/MacOS/Wireless360Controller
+# xcrun lipo -info build/Release/WirelessGamingReceiver.kext/Contents/MacOS/WirelessGamingReceiver
+# xcrun lipo -info build/Release/Wireless360Controller.kext/Contents/MacOS/Wireless360Controller
 echo "** File signatures **"
 xcrun spctl -a -v build/Release/360Controller.kext
 xcrun spctl -a -v build/Release/360Controller.kext/Contents/PlugIns/Feedback360.plugin
 xcrun spctl -a -v build/Release/360Daemon.app/Contents/MacOS/360Daemon
 xcrun spctl -a -v build/Release/Pref360Control.prefPane
 xcrun spctl -a -v build/Release/Pref360Control.prefPane/Contents/Resources/DriverTool
-xcrun spctl -a -v build/Release/WirelessGamingReceiver.kext
-xcrun spctl -a -v build/Release/Wireless360Controller.kext
+# xcrun spctl -a -v build/Release/WirelessGamingReceiver.kext
+# xcrun spctl -a -v build/Release/Wireless360Controller.kext
 xcrun spctl -a -v --type install Install360Controller/build/Install360Controller.pkg
 echo "*** DONE ***"
