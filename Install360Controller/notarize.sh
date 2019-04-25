0c52c1234687f72d57507ef9100b893bd8c1c925
Drew Mills <franticrain@gmail.com>
2019-04-25 18:38:45 -0500

    Update the notarization tools

--- Install360Controller/notarize.sh
+++ Install360Controller/notarize.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-# TODO(Drew): Update README to note changes to DeveloperSettings file
-
 DEV_EMAIL=`echo | grep DEVELOPER_EMAIL ../DeveloperSettings.xcconfig`
 NOTARIZATION_PASSWORD=`echo | grep NOTARIZATION_PASSWORD ../DeveloperSettings.xcconfig`
 DMG_NAME=`echo | ls | grep '360ControllerInstall.*.dmg'`
+USERNAME="${DEV_EMAIL//\DEVELOPER_EMAIL = }"
+PASSWORD="${NOTARIZATION_PASSWORD//\NOTARIZATION_PASSWORD = }"
 
-AUTHENTICATION="--username \"${DEV_EMAIL//\DEVELOPER_EMAIL = }\" --password \"${NOTARIZATION_PASSWORD//\NOTARIZATION_PASSWORD = }\""
+xcrun altool --notarize-app --primary-bundle-id "com.mice.driver" --username ${USERNAME} --password ${PASSWORD} --file ${DMG_NAME}
 
-xcrun altool --notarize-app --primary-bundle-id "com.mice.driver" ${AUTHENTICATION} --file ${DMG_NAME}
-xcrun stapler staple ${DMG_NAME}
-xcrun stapler validate ${DMG_NAME}
